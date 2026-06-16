17e94690060a8163ad097e6754aa1d03b2599bb5
kouovo <kou@linux.do>
2026-06-16 21:15:15 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn main(init: std.process.Init) !void {
     var stdout = std.Io.File.stdout().writer(init.io, &.{});
     var stdin_buffer: [4096]u8 = undefined;
-    var stdin = std.Io.File.stdin().readStreaming(init.io, &stdin_buffer);
+    var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
 
     const command = try stdin.interface.takeDelimiter('\n');
 
