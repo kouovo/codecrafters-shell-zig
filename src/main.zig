893177b9af88b5b2f8ee146c6e7bec9693c87bb2
Hoan <liamhuangzzz@gmail.com>
2026-07-21 10:03:47 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,10 @@ pub fn main(init: std.process.Init) !void {
         try stdout.interface.print("$ ", .{});
         const command = try stdin.interface.takeDelimiter('\n');
 
+        if (std.mem.eql(u8, command.?, "exit")) {
+            break;
+        }
+
         try stdout.interface.print("{s}: command not found\n", .{command.?});
     }
 }
