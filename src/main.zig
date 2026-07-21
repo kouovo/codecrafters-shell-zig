7cddd144e3f7fc2eb635dd9897f2a63d9e8d9941
Hoan <liamhuangzzz@gmail.com>
2026-07-21 10:37:15 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -8,10 +8,29 @@ pub fn main(init: std.process.Init) !void {
     while (true) {
         try stdout.interface.print("$ ", .{});
         const command = try stdin.interface.takeDelimiter('\n');
+        const cmd = command orelse break;
+        var args_iter = std.mem.tokenizeSequence(u8, cmd, " ");
+        const name = args_iter.next().?;
 
-        if (std.mem.eql(u8, command.?, "exit")) {
+        // while (args_iter.next()) |item| {
+        //     std.debug.print("args: '{s}'\n", .{item});
+        // }
+
+        if (std.mem.eql(u8, name, "exit")) {
             break;
+        } else if (std.mem.eql(u8, name, "echo")) {
+            // std.debug.print("{s} ", .{cmd[1..cmd.len]});
+            while (args_iter.next()) |item| {
+                std.debug.print("{s} ", .{item});
+            }
+
+            std.debug.print("\n", .{});
+            continue;
         }
+        // else if (std.mem.eql()) {
+        //     // const args = cmd[1..cmd.len];
+        //     // std.debug.print("{s}\n", .{args});
+        // }
 
         try stdout.interface.print("{s}: command not found\n", .{command.?});
     }
