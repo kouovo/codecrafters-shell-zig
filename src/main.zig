ddedc5226c0c32b7a77f340f8b579986d8316195
Hoan <liamhuangzzz@gmail.com>
2026-07-21 12:34:26 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,7 @@ fn parseBuiltin(name: []const u8) Builtin {
 fn findExecutable(io: std.Io, gpa: std.mem.Allocator, path_env: []const u8, name: []const u8) !?[]u8 {
     var dirs = std.mem.tokenizeScalar(u8, path_env, ':');
     while (dirs.next()) |dir| {
+        std.debug.print("{s}\n", .{dir});
         if (dir.len == 0) continue;
         const full = try std.fs.path.join(gpa, &.{ dir, name });
         errdefer gpa.free(full);
