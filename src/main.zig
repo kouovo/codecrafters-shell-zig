c1cd37010fc4c47e496e633cb6f3edaadf4c507c
Hoan <liamhuangzzz@gmail.com>
2026-07-21 09:49:22 +0800

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -5,8 +5,8 @@ pub fn main(init: std.process.Init) !void {
     var stdin_buffer: [4096]u8 = undefined;
     var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
 
+    try stdout.interface.print("$ ", .{});
     const command = try stdin.interface.takeDelimiter('\n');
 
-    try stdout.interface.print("$ ", .{});
     try stdout.interface.print("{s}: command not found\n", .{command.?});
 }
